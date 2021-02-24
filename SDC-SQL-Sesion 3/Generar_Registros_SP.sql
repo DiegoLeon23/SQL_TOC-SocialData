ccd556ca080faa3d68c6a0c8e4332b5f67aefa43
Diego Enrique Leon Ortiz <diego.leon1@unmsm.edu.pe>
2021-02-24 18:05:14 -0500

    Creacion de SP para generar registros _uso de DB institute

---
+++ SDC-SQL-Sesion 3/Generar_Registros_SP.sql
@@ -0,0 +1,29 @@
+ ---inserciones automaticas a tablas (qualification, assistance,registry_detail) a traves de SP
+ CREATE PROCEDURE SP_registro_Detalle_insert(
+ @id_registry int,
+ @id_student int 
+ )
+ as
+	 INSERT INTO registry_detail(id_registry,id_student,create_at,update_at)
+	 VALUES(@id_registry,@id_student,GETDATE(),GETDATE())
+
+	 declare @id_registry_detail int =@@identity
+	 INSERT INTO qualification(id_registry_detail,create_at,update_at)
+	 VALUES(@id_registry_detail,GETDATE(),GETDATE());
+ 
+	 declare @date  datetime = getdate();
+	 declare @date_end  datetime = dateadd(week,12,getdate());
+
+	 while @date<@date_end 
+	 begin
+		insert into assistance (id_registry_detail,[date],update_at)
+		values (@id_registry_detail,@date,getdate())
+		set @date = dateadd(week,1,@date);
+ end
+
+ execute SP_registro_Detalle_insert 1,1;
+
+ select * from assistance
+ select * from qualification
+ select * from registry_detail
+ select * from student
